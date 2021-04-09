f1254e41fd360efb2153c515a97f3b8f40c70b33
alan-mak <alanmak95@gmail.com>
2021-04-09 12:16:01 -0400

    Added db

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg')
 require('dotenv').config();
-const express = require("express");
+
 const app = express();
 
-
 const PORT = process.env.PORT || 8080;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: 'localhost',
+  database: 'aircanada'
+})
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send("HELLO");
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO location (city) VALUES ('Toronto');
+INSERT INTO location (city) VALUES ('Vancouver');
+INSERT INTO location (city) VALUES ('Montreal');

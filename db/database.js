eba09e70cd73b17197fb966a508f7abfb25d1701
alan-mak <alanmak95@gmail.com>
2021-04-09 21:43:43 -0400

    Filter by location id

--- db/database.js
+++ db/database.js
@@ -23,9 +23,25 @@ exports.locations = locations;
 
 const addWeather = (data) => {
   return pool.query(`
-  INSERT INTO weathers (lon, lat, temp, feels_like, temp_min, temp_max, pressure, humidity, visibility, wind_speed, wind_deg, last_update)
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, NOW());
-  `, [data.coord.lon, data.coord.lat, data.main.temp, data.main.feels_like, data.main.temp_min, data.main.temp_max, data.main.pressure, data.main.humidity, data.visibility, data.wind.speed, data.wind.deg]);
+  INSERT INTO weathers (lon, lat, temp, feels_like, temp_min, temp_max, pressure, humidity, visibility, wind_speed, wind_deg, last_update, location_id)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, NOW(), 1);
+  `, [data.coord.lon, data.coord.lat, data.main.temp, data.main.feels_like, data.main.temp_min, data.main.temp_max, data.main.pressure, data.main.humidity, data.visibility, data.wind.speed, data.wind.deg])
+  .catch(err => {
+    console.log(err);
+  });
 };
 exports.addWeather = addWeather;
 
+const showWeather = (id) => {
+  return pool.query(`
+  SELECT * FROM weathers
+  WHERE location_id = $1;
+  `, [id])
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+exports.showWeather = showWeather;
--- routes/location.js
+++ routes/location.js
@@ -8,13 +8,19 @@ module.exports = (router, database, weather) => {
   });
   
   router.get('/:id', (req, res) => {
+    //Get Weather of ID
     weather.getWeather()
     .then((data) => {
-      res.send(data)
       database.addWeather(data)
     })
     .catch(err => {
       console.log(err)
+    })
+
+    //Show weather of specific id page
+    database.showWeather(req.params.id)
+    .then(data => {
+      res.send(data)
     })
   });
 };
